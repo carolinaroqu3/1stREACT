9a4051ec9479c32da1d654b42c582d83f1bdf52f
carolinaroqu3 <44771728+carolinaroqu3@users.noreply.github.com>
2021-01-16 15:23:06 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <ul className="lista">
+        <li>learn react</li>
+        <li>yeah</li>
+      </ul>
+    </div>
+  );
+}
